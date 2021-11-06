f29bc94789804564c1b9da8c9b71d5acf258784c
Milo <50248166+Milo123459@users.noreply.github.com>
2021-11-06 12:43:51 +0000

    feat: examples: add example for take get_mut and into_inner

--- examples/mutex_cell.rs
+++ examples/mutex_cell.rs
@@ -1,7 +1,7 @@
 use with_lock::MutexCell;
 
 fn main() {
-	let a = MutexCell::new(2);
+	let mut a = MutexCell::new(2);
 	let b = MutexCell::new(3);
 	println!("{:?}", a.get() + b.get()); // 5
 	b.set(4);
@@ -10,4 +10,11 @@ fn main() {
 	println!("A: {:?} B: {:?}", a.get(), b.get()); // A: 4 B: 2
 	a.replace(4);
 	println!("{:?}", a.get() + b.get()); // 8
+	let cell = a.get_mut();
+	*cell += 1;
+	println!("{:?}", a.get()); // 5
+
+	let a_new = a.take();
+	println!("A: {:?}", a.into_inner());
+	println!("A_NEW: {:?}", a_new);
 }
